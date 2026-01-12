439c6f70e274c5cbe6408e5b9b06f59db76962ae
Demo <kartikkumbhar1812@gmail.com>
2026-01-12 11:53:26 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(cors({
   credentials: true
 }));
 
-// app.options("/*", cors());
+app.options("/*", cors());
 
 app.use(express.json({ limit: "4mb" }));
 app.use(express.urlencoded({ extended: true }));
