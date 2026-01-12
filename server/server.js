3db48a66550b8609fe4a2561764036e71aa366bb
Demo <kartikkumbhar1812@gmail.com>
2026-01-12 12:16:42 +0530

    pack

--- server/server.js
+++ server/server.js
@@ -10,37 +10,24 @@ import { Server } from "socket.io";
 const app = express();
 const server = http.createServer(app);
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://chat-app-frontend-pink-nu.vercel.app" 
-];
-
-// Middleware
 app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://chat-app-frontend-pink-nu.vercel.app"
   ],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization","token"]
+  credentials: true
 }));
-
-app.options("*", cors());
 
 app.use(express.json({ limit: "4mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-// DB
 connectDB();
 
-// Routes
 app.use("/auth", userRouter);
 app.use("/messages", messageRouter);
 
 app.get("/", (req, res) => res.send("Server is Live"));
 
-// SOCKET.IO
 export const io = new Server(server, {
   cors: {
     origin: [
@@ -55,24 +42,17 @@ export const userSocketMap = {};
 
 io.on("connection", (socket) => {
   const userId = socket.handshake.query?.userId;
-  console.log("User Connected:", userId);
-
   if (userId) userSocketMap[userId] = socket.id;
 
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
-    console.log("User Disconnected:", userId);
     delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
 
-// START SERVER 
-// if (process.env.NODE_ENV != "production") {
-//   const PORT = process.env.PORT || 5000;
-//   server.listen(PORT, () => console.log("Server running on PORT " + PORT));
-// }
-
-export default server;
-
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () =>
+  console.log("Server running on PORT", PORT)
+);
